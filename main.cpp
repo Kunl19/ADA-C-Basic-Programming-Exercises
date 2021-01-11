e108a644641f8e8896e1c5e7cdb40accf3b0b6e8
Ayokunle Elerewe <ayokunle.elerewe@ada.ac.uk>
2021-01-11 12:24:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//https://repl.it/@Kunl19/ADA-1-QuoteAQuote
+
+
+#include <iostream>
+
+
+int main() {
+  std::string quote;
+  std::string author;
+
+  std::cout << "What is the quote\n";
+  //std::cin >> quote;
+  getline(std::cin, quote);
+
+  std:: cout << "Who said it\n";
+  //std::cin >> author;
+  getline(std::cin , author);
+
+  std::cout << "\n" << author << " says: \"" << quote << "\"\n";
+
+	return 0;
+
+}
